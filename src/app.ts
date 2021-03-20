6dd8f591ff9b63b35ff92c7416a443979cd04a92
Tanay Raj <tanay.raj76@gmail.com>
2021-03-20 22:20:38 +0530

    full branch name added

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,31 @@ class VITRegNumber {
       if (/^\d+$/.test(branch)) {
         throw new Error("invalid characters in branch code");
       } else {
-        this.branch = branch;
+        if(branch === 'BCE')
+        this.branch = branch+"(Computer Science Engineering Core)"
+        else if(branch === 'BBS')
+        this.branch = branch+"(Computer Science Engineering with Specialization in Business Systems)"
+        else if(branch === 'BCB')
+        this.branch = branch+"(Computer Science Engineering specialization in Bioinformatics)"
+        else if(branch === 'BCD')
+        this.branch = branch+"(Computer Science Specialisation in Data Analytics)"
+        else if(branch === 'BCI')
+        this.branch = branch+"(Computer Science with specialization in information security)"
+        else if(branch === 'BCN')
+        this.branch = branch+"(Computer Science specialization in Networking and security)"
+        else if(branch === 'BCT')
+        this.branch = branch+"(Computer Science Engineering with Specialization in Internet of Things)"
+        else if(branch === 'BEC')
+        this.branch = branch+"(Electronics and Communication Engineering)"
+        else if(branch === 'BEE')
+        this.branch = branch+"(Electrical and Electronics engineering)"
+        else if(branch === 'BIT')
+        this.branch = branch+"(Information Technology)"
+        else if(branch === 'BAI')
+        this.branch = branch+"(Computer Science Engineering with Specialization in Artificial Intelligence and Machine Learning)"
+        else
+        this.branch = branch
+
       }
     } catch (e) {
       throw new Error(`Invalid Registration Number :: ${e.message}`);
